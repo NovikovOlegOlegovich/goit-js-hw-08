c0ec9f468ad35137f540ec4bc4a137412648e08b
Novikov Oleh <novikov19962906@gmail.com>
2023-04-14 02:04:27 +0300

    prepare error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,12 @@ import _throttle from 'lodash.throttle';
 
 const timeVideoKey = 'videoplayer-current-time';
 
-iframeEl = document.querySelector('iframe');
+const iframeEl = document.querySelector('iframe');
 
 const player1 = new player(iframeEl);
 
-
 player1.on('timeupdate', _throttle(onPlay, 1000));
 setCurrentTime();
-
 
 function onPlay(event) {
   localStorage.setItem(timeVideoKey, event.seconds);
