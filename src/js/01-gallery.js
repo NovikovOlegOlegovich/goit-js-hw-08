a3b48932b679a52dee1319c90d40e1911786ce47
Novikov Oleh <novikov19962906@gmail.com>
2023-04-13 12:21:48 +0300

    new file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+
+const modal = new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+// const player = require('@vimeo/player');
+import player from '@vimeo/player';
 
+iframeEl = document.querySelector('iframe');
+
+const player1 = new Vimeo.Player(iframe);
+
+player1.on('play', onPlay);
+
+function onPlay(event) {
+  console.log(event.seconds);
+}
